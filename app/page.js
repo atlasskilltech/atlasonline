9f4fe07dd66c234c83caa22d9cd56bb2241f0e74
SushantAmin17 <sushantamin81@gmail.com>
2026-02-17 11:55:24 +0530

    Changes commit

--- app/page.js
+++ app/page.js
@@ -28,7 +28,6 @@ export default function Home() {
       <HeroBanner />
       <ProgramOverview/>
       <ProgramChoose/>
-      <Wealth/>
       <CanTakeYou/>
       <BuiltAmbitious/>
       <PathToGlobal/>
