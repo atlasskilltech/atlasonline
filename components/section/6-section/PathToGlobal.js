69461c389ba35265251327fd5348167294e32a63
faizanshaikhatlas <faizan.shaikh@atlasuniversity.edu.in>
2026-02-20 17:32:07 +0530

    pushing section 6

--- components/section/6-section/PathToGlobal.js
+++ components/section/6-section/PathToGlobal.js
@@ -2,29 +2,133 @@
 
 const Wealth = () => {
   return (
-    <section className="
-   w-full   px-[24px] py-[40px] bg-white  
-   small:pl-[60px] small:pr-[40px] 
-   medium:py-[50px]
-   
-   base:pl-[80px] base:pr-[70px]  base:py-[70px]
-
-   large:px-[140px]   large:py-[80px]
-   ">
-      <div className="mx-auto">
-        <header>
-          <p className="text-[24px] text-[#666] font-semibold  small:text-[28px] medium:text-[30px] base:text-[40px] cursor-(--f-cursor-text)
-      ">    
-          Your Path To Global
+    <section
+    className="
+ w-full px-[24px] py-[40px] bg-white  
+ small:pl-[60px] small:pr-[40px] 
+ medium:py-[60px]  medium:pr-[60px] 
+ base:pl-[80px] base:pr-[70px]  base:py-[70px]
+ large:px-[140px]   large:py-[80px]
+ "
+  >
+    <div className="flex flex-col gap-[24px]">
+      <div >
+        <p
+          className="text-[24px] text-[#222222] font-semibold  small:text-[28px] medium:text-[30px] base:text-[40px] cursor-(--f-cursor-text)
+    "
+        >
+          Your Path to Global
         </p>
-
-        <h1 className="text-[32px] font-bold font-denton leading-tight  text-transparent bg-clip-text bg-gradient-to-r from-[#2c2c7a] to-[#14b8a6] small:text-[44px] medium:text-[54px] base:text-[64px] cursor-(--f-cursor-text)">
-            Financial Planning Excellence
+        <h1 className="text-[32px] font-bold font-denton leading-tight  bg-[radial-gradient(248.29%_125.44%_at_6.23%_19.77%,_#342B7C_0%,_#02A7B6_100%)]
+  bg-clip-text
+  text-transparent  small:text-[44px] medium:text-[54px] base:text-[64px] cursor-(--f-cursor-text)">
+        Financial Planning Excellence
         </h1>
-        </header>
-
       </div>
-    </section>
+
+
+    
+           <div className="main">
+<div className="inline-flex justify-center items-center gap-4">
+  <div className="flex w-[60px] h-[60px] p-[10px] 
+flex-col justify-center items-center 
+gap-[10px] shrink-0 bg-gradient-to-b from-[#342B7C] to-[#02A7B6] text-[#fff]">
+  <p className="text-white 
+text-center text-[22px] 
+font-semibold 
+leading-[140%] 
+tracking-[0.22px]">01</p>
+
+</div>
+  <div className="flex flex-col items-start gap-[8px] flex-1">
+    <p className="text-black text-base font-semibold leading-6
+tracking-tight text-[16px]">Submit Application</p>
+    <p className="text-[14px] font-semibold text-[#000000B3]">Complete the application by providing the essential details about yourself.</p>
+  </div>
+</div>
+
+<div className="inline-flex justify-center items-center gap-4">
+  <div className="flex w-[60px] h-[60px] p-[10px] 
+flex-col justify-center items-center 
+gap-[10px] shrink-0 bg-white">
+ <img src="/shape/line.png" alt="" />
+
+</div>
+ 
+</div>   
+<div className="inline-flex justify-center items-center gap-4">
+  <div className="flex w-[60px] h-[60px] p-[10px] 
+flex-col justify-center items-center 
+gap-[10px] shrink-0 bg-gradient-to-b from-[#342B7C] to-[#02A7B6] text-[#fff]">
+  <p className="text-white 
+text-center text-[22px] 
+font-semibold 
+leading-[140%] 
+tracking-[0.22px]">02</p>
+
+</div>
+  <div className="flex flex-col items-start gap-[8px] flex-1">
+    <p className="text-black text-base font-semibold leading-6
+tracking-tight text-[16px]">Reserve Your Seat</p>
+    <p className="text-[14px] font-semibold text-[#000000B3]">
+    Secure your seat by completing the program fee payment.
+    </p>
+  </div>
+</div>
+<div className="inline-flex justify-center items-center gap-4">
+  <div className="flex w-[60px] h-[60px] p-[10px] 
+flex-col justify-center items-center 
+gap-[10px] shrink-0 bg-white">
+ <img src="/shape/line.png" alt="" />
+
+</div>
+ 
+</div> 
+<div className="inline-flex justify-center items-center gap-4">
+  <div className="flex w-[60px] h-[60px] p-[10px] 
+flex-col justify-center items-center 
+gap-[10px] shrink-0 bg-gradient-to-b from-[#342B7C] to-[#02A7B6] text-[#fff]">
+  <p className="text-white 
+text-center text-[22px] 
+font-semibold 
+leading-[140%] 
+tracking-[0.22px]">02</p>
+
+</div>
+  <div className="flex flex-col items-start gap-[8px] flex-1">
+    <p className="text-black text-base font-semibold leading-6
+tracking-tight text-[16px]">Start Learning</p>
+    <p className="text-[14px] font-semibold text-[#000000B3]">
+    Begin your learning journey on the designated cohort start date.
+    </p>
+  </div>
+</div>
+          </div>
+
+
+
+
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  </section>
   );
 };
 
